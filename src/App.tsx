ffa1617b88ae46f8302b3d420aa61da9bb3de2bd
alex <alexey.chuprov@gmail.com>
2021-12-24 08:37:00 +0300

    PostsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./ex01";
 import { Counter } from "./features/counter/Counter"
+import {PostsList} from "./features/posts/PostsList"
+
 export const App = () => <div>
     <h1>Hello Redux</h1>
     <Counter />
+    <PostsList/>
 </div>
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,11 +1,10 @@
 // import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
-// import styles from './Counter.module.css'
 
 export function Counter() {
   const count = useSelector(state => 
-    state.value)
+    state.counter.value)
   const dispatch = useDispatch()
 
   return (
