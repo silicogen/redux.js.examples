433f265fbfb02627623696cc3f5287a53fd4e7dd
alex <alexey.chuprov@gmail.com>
2021-12-24 07:47:51 +0300

    pretty

--- src/ex01.js
+++ src/ex01.js
@@ -1,22 +1,22 @@
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
 function counterReducer(state = { value: 0 }, action) {
   switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
+    case "counter/incremented":
+      return { value: state.value + 1 };
+    case "counter/decremented":
+      return { value: state.value - 1 };
     default:
-      return state
+      return state;
   }
 }
 
-let store = createStore(counterReducer)
+let store = createStore(counterReducer);
 
-store.subscribe(() => console.log(store.getState()))
+store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({ type: 'counter/incremented' });
+store.dispatch({ type: "counter/incremented" });
 
-store.dispatch({ type: 'counter/incremented' });
+store.dispatch({ type: "counter/incremented" });
 
-store.dispatch({ type: 'counter/decremented' });
+store.dispatch({ type: "counter/decremented" });
